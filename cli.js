8537f7902ab2e141956fd3588fbdd728085dd63c
Влад KaMeHb Марченко <blade.whiteblack@gmail.com>
2017-12-10 04:31:39 +0200

    Initial commit

---
+++ cli.js
@@ -0,0 +1,25 @@
+﻿console.log('Starting...');
+const {spawnSync} = require('child_process');
+let safeRequire = (() => {
+    try{
+        return require('extd-require');
+    } catch(e){
+        console.log('Installing extd-require...');
+        if (spawnSync('npm', ['install', modulename.split('/')[0]], {
+            cwd: mainDir,
+            stdio: 'ignore'
+        }).status){
+            throw new ReferenceError('Cannot install extd-require');
+        }
+        return require('extd-require');
+    }
+})();
+
+safeRequire('loading-animation').then(LoadingAnimation => {
+    console.log('Required loading-animation');
+    safeRequire('nodemailer').then(nodemailer => {
+        console.log('Required nodemailer');
+    });
+});
+//LoadingAnimation = safeRequire('loading-animation');
+//const nodemailer = safeRequire('nodemailer');
